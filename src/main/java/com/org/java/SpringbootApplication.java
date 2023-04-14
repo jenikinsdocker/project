38b172353c88f2f06f889096ac53a3c6e7e37ab8
jenikinsdocker <jenikinsdocker@gmail.com>
2023-04-14 10:47:45 +0530

    addsd

--- src/main/java/com/org/java/SpringbootApplication.java
+++ src/main/java/com/org/java/SpringbootApplication.java
@@ -15,7 +15,6 @@ public class SpringbootApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
-		System.out.println("hello");
 	}
 	
 
